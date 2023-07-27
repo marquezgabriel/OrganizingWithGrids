3a144a1623e32ea79c4b3475f542e369874705d6
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2023-07-27 09:42:08 -0400

    Initial Commit

---
+++ OrganizingWithGrids/OrganizingWithGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizingWithGridsApp.swift
+//  OrganizingWithGrids
+//
+//  Created by Gabriel Marquez on 2023-07-27.
+//
+
+import SwiftUI
+
+@main
+struct OrganizingWithGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
